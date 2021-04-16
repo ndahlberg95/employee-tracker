1e999cb29d7a263a4d82ca5d67e7a5f96e7ee410
Nora Dahlberg <ndahlberg95@gmail.com>
2021-04-15 18:34:16 -0600

    add department, role, and employee routes

---
+++ routes/apiRoutes/departmentRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+// Get all departments
+router.get('/roles', (req, res) => {
+  const sql = `SELECT * FROM departments`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+module.exports = router;
---
+++ routes/apiRoutes/employeeRoutes.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+// Get all employees and their role
+router.get('/employees', (req, res) => {
+  const sql = `SELECT employees.*, role.name 
+                AS role 
+                FROM employees
+                LEFT JOIN role 
+                ON employees.role = role.id`;
+
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Create a Employee
+router.post('/Employee', ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'role',
+    "manager"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `INSERT INTO employees (first_name, last_name, role, manager) VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.role,
+    body.manager
+  ];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Get single employee
+router.get('/employee/:id', (req, res) => {
+  const sql = `SELECT * FROM employees WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Update a Employee's role
+router.put('/Employee/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'role');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE employees SET role = ? 
+               WHERE id = ?`;
+  const params = [req.body.role, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Employee not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+module.exports = router;

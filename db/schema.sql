b6e64be3846ecf116ab997efd0aea3ba3b06d856
Nora Dahlberg <ndahlberg95@gmail.com>
2021-04-18 15:15:12 -0600

    update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,40 +1,22 @@
-DROP TABLE IF EXISTS votes;
-DROP TABLE IF EXISTS candidates;
-DROP TABLE IF EXISTS parties;
-DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
 
-CREATE TABLE parties (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
-
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employees (
+  id INT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party
-    FOREIGN KEY (party_id)
-    REFERENCES parties(id)
-    ON DELETE SET NULL
+  role_id VARCHAR(30) NOT NULL,
+  manager_id INTEGER,
 );
 
-CREATE TABLE voters (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE departments (
+  id INT PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE votes (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  voter_id INTEGER NOT NULL,
-  candidate_id INTEGER NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT uc_voter UNIQUE (voter_id),
-  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
-  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+CREATE TABLE roles (
+  id INT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INTEGER
 );
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -191,21 +191,3 @@ promptUser()
     });
 
 module.exports = router;
-
-
-//BELOW IS SQL COMMANDS TO BUILD DATABSE/TABLES:
-
-//CREATE DATABASE company;
-//USE company;
-
-// CREATE TABLE employees (
-//     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-//     first_name VARCHAR(30) NOT NULL,
-//     last_name VARCHAR(30) NOT NULL,
-//     role VARCHAR(30) NOT NULL,
-//     manager INTEGER,
-//     departmentID INTEGER);
-
-// CREATE TABLE departments (
-//     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-//     name VARCHAR(30) NOT NULL);
